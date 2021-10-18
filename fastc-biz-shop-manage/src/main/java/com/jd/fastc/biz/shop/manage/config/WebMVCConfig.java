c1f24f0ed4b20cb09d42b903e12623f373bb570b
ext.yejianjun1 <ext.yejianjun1@jd.com>
2021-10-18 15:03:12 +0800

    支持Jmock log

---
+++ fastc-biz-shop-manage/src/main/java/com/jd/fastc/biz/shop/manage/config/WebMVCConfig.java
@@ -0,0 +1,27 @@
+package com.jd.fastc.biz.shop.manage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/***
+ * @Auther: yejianjun
+ * @Date: 2021/10/18
+ * @Title:
+ *
+ */
+
+@Configuration
+public class WebMVCConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+}
