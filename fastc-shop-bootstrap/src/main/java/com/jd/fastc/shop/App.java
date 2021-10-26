0029db9a3c382a1bddf3bf3c1e33b4f6a4b3fc30
ext.yejianjun1 <ext.yejianjun1@jd.com>
2021-10-26 17:43:12 +0800

    单元测试

--- fastc-shop-bootstrap/src/main/java/com/jd/fastc/shop/App.java
+++ fastc-shop-bootstrap/src/main/java/com/jd/fastc/shop/App.java
@@ -1,4 +1,4 @@
-package com.jd.fastc.shop.boostrap;
+package com.jd.fastc.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
