b90cbdb470338954763dbf06f048ba1f3171e178
ext.yejianjun1 <ext.yejianjun1@jd.com>
2021-11-10 09:48:58 +0800

    评审修改

---
+++ fastc-biz-shop-manage/src/main/java/com/jd/fastc/biz/shop/manage/enums/ConstantCode.java
@@ -0,0 +1,37 @@
+package com.jd.fastc.biz.shop.manage.enums;
+
+/***
+ * @Auther: yejianjun
+ * @Date: 2021/11/10
+ * @Title:
+ *
+ */
+
+public enum ConstantCode {
+
+    /**
+     * Web browser on PC
+     */
+    PC(1),
+    /**
+     * 1.有货 0.无货
+     */
+    IN_Stock(1),
+
+    NO_Stock(0);
+
+    private Integer code;
+
+    ConstantCode(Integer code) {
+        this.code = code;
+    }
+
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
